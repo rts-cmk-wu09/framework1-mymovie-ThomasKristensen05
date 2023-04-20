0a9656301caf9f5ce10f650bca207403155379e0
ThomasKristensen05 <thomas12andre@gmail.com>
2023-04-20 14:07:31 +0200

    add  all the good stuff

---
+++ src/components/HeadingOne.jsx
@@ -0,0 +1,11 @@
+const HeadingOne = () => {
+
+    return (
+        
+        <h1>Overskrift</h1>
+        
+    )
+
+}
+
+export default HeadingOne
---
+++ src/components/HeadingTwo.jsx
@@ -0,0 +1,7 @@
+const HeadingTwo = () => {
+    return ( 
+        <h2>Now Showing</h2>
+     );
+}
+ 
+export default HeadingTwo;
